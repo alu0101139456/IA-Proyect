e24a74c2e618619ca36642ebf2b52483ae829509
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-11-08 17:22:20 +0000

    Se mueven atributos a la parte superior de la clase Se añaden metodos: EliminarDeListasVecinos update_vecinos isInMalla

--- include/tablero.hpp
+++ include/tablero.hpp
@@ -6,7 +6,28 @@
 #include <SFML/Graphics.hpp>
 #include "celda.hpp"
 
+// estados:
+// 1 -> Inicio
+// 2 -> Final
+// 3 -> Obstaculo
+
+
+
 class Tablero : public sf::Drawable {
+ private:
+  std::vector<int> f = {-1, 0, 1, 0};//Coordenadas para los vecinos
+  std::vector<int> c = { 0,-1, 0, 1};
+  int filas_;
+  int columnas_;
+  int i_inicial = -1;
+  int j_inicial = -1;
+  int i_final = -1;
+  int j_final = -1;
+  Celda** malla_;
+  float celdas_size_;
+  void position_cursor(int &i, int &j);
+  int position(int i, int j) const;
+
  public:
   Tablero(void);
   Tablero(int filas, int columnas);
@@ -15,20 +36,13 @@ class Tablero : public sf::Drawable {
   void set_final(int i, int j);
   void set_obstaculo(int i, int j);
   void modo_aleatorio(int num_obstaculos);
- void redimensionar(sf::RenderWindow& window);
+  void redimensionar(sf::RenderWindow& window);
   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+  void EliminarDeListasVecinos(Celda* celda_obstaculo);
+  void update_vecinos( int i, int j);
+  bool isInMalla(int i, int j);
 
- private:
-  int filas_;
-  int columnas_;
-  int i_inicial;
-  int j_inicial;
-  int i_final;
-  int j_final;
-  Celda** malla_;
-  float celdas_size_;
-  void position_cursor(int &i, int &j);
-  int position(int i, int j) const;
+
 };
 
 #endif
