a14147c4704318a1e89c6e061248a48684162df5
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-11-08 20:43:30 +0000

    Se cambian referencias por punteros

--- include/heuristica.hpp
+++ include/heuristica.hpp
@@ -8,25 +8,23 @@ class FunHeuristica {
  public:
   FunHeuristica();
   virtual ~FunHeuristica();
-  virtual int operator()(const Celda&, const Celda&) = 0;
+  virtual int operator()(const Celda*, const Celda*) = 0;
 };
 
 
-//-----------------------DISTANCIA EUCLIDEA-------------------------
 
 class DistanciaEuclidea : public FunHeuristica {
  public:
   DistanciaEuclidea();
-  int operator()(const Celda&, const Celda&);
+  int operator()(const Celda*, const Celda*);
 };
 
 
-//-----------------------DISTANCIA RECTILINEA-----------------------
 
 class DistanciaManhattan : public FunHeuristica {
  public:
   DistanciaManhattan();
-  int operator()(const Celda&, const Celda&);
+  int operator()(const Celda*, const Celda*);
 };
 
 #endif // F_HEURISTICAS_H
--- src/heuristica.cpp
+++ src/heuristica.cpp
@@ -5,18 +5,16 @@ FunHeuristica::FunHeuristica(){}
 
 FunHeuristica::~FunHeuristica(){}
 
-//-----------------------------------------------------
 
 DistanciaEuclidea::DistanciaEuclidea(): FunHeuristica(){}
 
-int DistanciaManhattan::operator()(const Celda& i, const Celda& f){
-    return static_cast<int>(sqrt(pow((i.Get_i() - f.Get_i()),2) + pow((i.Get_j() - f.Get_j()), 2)));
+int DistanciaEuclidea::operator()(const Celda* i, const Celda* f){
+    return static_cast<int>(sqrt(pow((i->Get_i() - f->Get_i()),2) + pow((i->Get_j() - f->Get_j()), 2)));
 }
 
-//-----------------------------------------------------
 
 DistanciaManhattan::DistanciaManhattan(): FunHeuristica(){}
 
-int DistanciaManhattan::operator()(const Celda& i, const Celda& f){
-    return static_cast<int>(fabs(static_cast<double>(i.Get_i() - f.Get_i())) + fabs(static_cast<double>(i.Get_j() - f.Get_j())));
+int DistanciaManhattan::operator()(const Celda* i, const Celda* f){
+    return static_cast<int>(fabs(static_cast<double>(i->Get_i() - f->Get_i())) + fabs(static_cast<double>(i->Get_j() - f->Get_j())));
 }
