09b267e9152d6cf1f66ca4e6cffb556df2ae5cd9
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-11-12 22:44:51 +0000

    Corregida ubicaciones de fiuchero

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(void) {
     std::string fichero;
     system("clear");
     std::cout << "Los ficheros \".txt\" que se pueden leer son: " << std::endl;
-    system("ls *.txt");
+    system("ls TestFiles/*.txt");
     std::cout << "Introduzca el nombre del fichero de entrada:  ";
     std::cin >> fichero;
     std::cout << std::endl;
